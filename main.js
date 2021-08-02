3449e6d1f8c69f5b55f3d80d38e25fbbd4ea0b4f
Carina Gonzalez <carinagonzalez@MacBook-Pro-de-Carina.local>
2021-08-01 20:58:23 -0500

    First project with VueJs

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      titulo: "Hola Carina",
+      enlace: "https://www.youtube.com/",
+      cantidad: 0,
+      estado: false,
+      servicios: ["transferencias", "pagos", "servicios", "cheques"],
+    };
+  },
+});
